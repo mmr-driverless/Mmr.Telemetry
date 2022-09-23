f8eb5d529bf47153022c523ad9e0e1a6ef909a1c
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-09-23 19:29:38 +0200

    Create daemon

---
+++ Mmr.Telemetry.Daemon/Program.cs
@@ -0,0 +1,10 @@
+using Mmr.Telemetry.Daemon;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
