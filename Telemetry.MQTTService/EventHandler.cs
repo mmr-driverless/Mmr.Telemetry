1c56762796e35493a44b3c0ba9d0bfd0b9c1898a
Andrea Magnanini <63046209+AndreaMagnanini@users.noreply.github.com>
2022-11-25 17:18:49 +0100

    added console application project to simulate an MQTT message receiver

---
+++ Telemetry.MQTTService/EventHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet.Client;
+using MQTTnet.Server;
+
+namespace Telemetry.MQTTService
+{
+    public class EventHandlerManager
+    {
+        public EventHandlerManager() { }
+
+        public Task OnConnectionCompleted(MqttClientConnectedEventArgs arg)
+        {
+            Console.WriteLine("Client connected!");
+            return Task.CompletedTask;
+        }
+
+        public void OnConnectionFailed()
+        {
+            Console.WriteLine("Client connection failed!");
+        }
+
+        public Task OnDisconnectedSuccessful(MqttClientDisconnectedEventArgs arg)
+        {
+            Console.WriteLine("Client disconnected!");
+            return Task.CompletedTask;
+        }
+
+        public Task OnMessageReceived(MqttApplicationMessageReceivedEventArgs arg)
+        {
+            Console.WriteLine("One message received!\n");
+            var text = Encoding.UTF8.GetString(arg.ApplicationMessage.Payload);
+            Console.WriteLine("TEXT OF THE MESSAGE:");
+            Console.WriteLine($"{text}\n");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Telemetry.MQTTService/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MQTTnet.Client;
+using MQTTnet;
+using Telemetry.MQTTService;
+
+var mqttFactory = new MqttFactory();
+IMqttClient client = mqttFactory.CreateMqttClient();
+var options = new MqttClientOptionsBuilder()
+    .WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("test.mosquitto.org", 1883)
+    .WithCleanSession()
+    .Build();
+
+var topicFilter = new MqttTopicFilterBuilder()
+    .WithTopic("mqtt.test")
+    .Build();
+
+var eventManager = new EventHandlerManager();
+client.ConnectedAsync += eventManager.OnConnectionCompleted;
+client.DisconnectedAsync += eventManager.OnDisconnectedSuccessful;
+client.ApplicationMessageReceivedAsync += eventManager.OnMessageReceived;
+
+await client.ConnectAsync(options);
+if (!client.IsConnected) eventManager.OnConnectionFailed();
+
+await client.SubscribeAsync(topicFilter);
+
+var startTimeSpan = TimeSpan.Zero;
+var periodTimeSpan = TimeSpan.FromSeconds(15);
+var timer = new Timer((e) =>
+{
+    Console.WriteLine("Waiting for published messages...");
+}, null, startTimeSpan, periodTimeSpan);
+
+while (true)
+{ }
+
+
