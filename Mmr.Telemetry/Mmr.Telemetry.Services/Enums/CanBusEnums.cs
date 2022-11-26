21652045cd2267a496094e0b32b42add06ea3533
Andrea Magnanini <63046209+AndreaMagnanini@users.noreply.github.com>
2022-11-26 16:22:32 +0100

    added mmr enums definitions as in excel provided document

---
+++ Mmr.Telemetry/Mmr.Telemetry.Services/Enums/CanBusEnums.cs
@@ -0,0 +1,33 @@
+﻿namespace Mmr.Telemetry.Services.Enums
+{
+    public class CanBusEnums
+    {
+
+        public static IDictionary<CanBusEnum, string> CanBusDictionary = new Dictionary<CanBusEnum, string>()
+        {
+            { CanBusEnum.Res, "RES" },
+            { CanBusEnum.Sensing, "S" },
+            { CanBusEnum.AutonomousState, "AS" },
+            { CanBusEnum.Mission, "M" },
+            { CanBusEnum.Drive, "D" },
+            { CanBusEnum.EcuBosch, "ECU" },
+            { CanBusEnum.ControlSignal, "CS" },
+            { CanBusEnum.Steering, "ST" },
+            { CanBusEnum.Brake, "BRK" },
+            { CanBusEnum.Logging, "L" },
+        };
+    }
+    public enum CanBusEnum
+    {
+        Res,
+        Sensing,
+        AutonomousState,
+        Mission,
+        Drive,
+        EcuBosch,
+        ControlSignal,
+        Steering,
+        Brake,
+        Logging
+    }
+}
