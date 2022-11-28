7c6108f7722d571b9a7a7fff0901ceefd6c617bf
OT\andrea.magnanini <andrea.magnanini@otconsulting.com>
2022-11-28 17:05:04 +0100

    added AutoMapper and Json NuGet packages

--- Mmr.Telemetry/Mmr.Telemetry.Services/Program.cs
+++ Mmr.Telemetry/Mmr.Telemetry.Services/Program.cs
@@ -1,8 +1,13 @@
+using Mmr.Telemetry.Services.mqtt;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IMqttTelemetryClient, MqttTelemetryClient>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
